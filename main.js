98b3c623006d34488b0f95ee3cd91c1babdf7196
Matthew Palermo <matt.r.palermo@gmail.com>
2018-03-12 14:00:06 +1000

    Fix script execution

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 const source = document.body.innerText;
 const processor = Asciidoctor({runtime: {platform: 'browser'}});
 const output = processor.convert(source, {header_footer: true});
-console.log(output);
 const parser = new DOMParser();
 const doc = parser.parseFromString(output, "text/html");
-document.body = doc.body;
-document.head.innerHTML = doc.head.innerHTML;
+// Using outerHTML results in NoModificationAllowedError error
+document.documentElement.innerHTML = doc.documentElement.innerHTML;
+// <script> elements parsed with DOMParser and innerHTML aren't executed.
+// Let's execute them with this simple method.
+Array.from(document.getElementsByTagName('script')).forEach((script) => {
+  // script.cloneNode(true) doesn't work. Copy them manually.
+  const newScript = document.createElement('script');
+  if (script.type) newScript.type = script.type;
+  if (script.src) {
+    newScript.src = script.src;
+  } else {
+    newScript.textContent = script.textContent;
+  }
+  script.parentNode.replaceChild(newScript, script);
+});
